5810b32de840bbff1d2f149a0d9efa93847d3451
Ben Castan <ben@castanfamily.com>
2018-03-17 21:18:31 +1100

    More chatpers covered

--- src/app.py
+++ src/app.py
@@ -5,4 +5,13 @@ request = requests.get("https://www.johnlewis.com/house-by-john-lewis-hinton-off
 content = request.content
 soup = BeautifulSoup(content, "html.parser")
 element = soup.find("span",{"itemprop": "price", "class":"now-price"})
-print(element.text.strip())
+string_prince = element.text.strip()
+price_without_symbol = string_prince[1:]
+price = float(price_without_symbol)
+
+if price > 200:
+    print("At {} this chair is too expensive!".format(string_prince))
+elif 200 > price > 100:
+    print("At {} this chair is affordable".format(string_prince))
+else:
+    print("At {} this chair is rubbish, don't buy it!".format(string_prince))
